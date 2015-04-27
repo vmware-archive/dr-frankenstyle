2cd98cd3bbabffaad270548c67eeb4fe52f512ee
Vinson Chuong <vchuong@pivotal.io>
2015-04-27 16:03:15 -0700

    Add stream option [#93130098]
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -3,6 +3,8 @@ import path from 'path';
 import childProcess from 'child_process';
 import rimraf from 'rimraf';
 import drFrankenstyle from '../index';
+import through2 from 'through2';
+import {readFile} from '../lib/async';
 
 let packageJson = {};
 
@@ -45,7 +47,6 @@ function writeCode(filePath, fn) {
 }
 
 describe('dr-frankenstyle', function() {
-  let css;
   beforeEach(function() {
     jasmine.addMatchers({
       toHaveOrder: () => {
@@ -71,42 +72,81 @@ describe('dr-frankenstyle', function() {
     let dependencyTree = d('myApp', [d('timeTravel'), d('delorean'), d('focus'), d('f150')]);
 
     writePackage(dependencyTree, __dirname);
-    writeCode(path.resolve(__dirname, 'myApp', 'index.js'), () => {
-      require('../..')(function(file, callback) {
-        if (file.path === 'components.css') console.log(file.contents.toString());
-        callback(null, file);
-      });
-    });
-
-    css = childProcess.spawnSync('node', ['index.js'], {cwd: path.resolve(__dirname, 'myApp')})
-      .stdout.toString();
   });
 
   afterEach(function() {
     rimraf.sync(path.resolve(__dirname, 'myApp'));
   });
 
-  it('has no duplicates', function() {
-    var rules = css.split('\n').filter(Boolean);
-    var expectedPackages = ['tires', 'brakes', 'calipers', 'drums', 'delorean', 'mr-fusion', 'focus', 'f150', 'truck-tires', 'cowboy-hat', 'truck-bed', 'gate', 'timeTravel', '88-mph'];
+  describe('without streaming', function() {
+    let css;
+    beforeEach(function() {
+      writeCode(path.resolve(__dirname, 'myApp', 'index.js'), () => {
+        require('../../index.js')(function(file, callback) {
+          if (file.path === 'components.css') console.log(file.contents.toString());
+          callback(null, file);
+        });
+      });
+      css = childProcess.spawnSync('node', ['index.js'], {cwd: path.resolve(__dirname, 'myApp')})
+        .stdout.toString();
+    });
 
-    for(var i = 0; i < expectedPackages.length; i++) {
-      expect(rules).toContain('.' + expectedPackages[i] + ' {color: red}');
-    }
-    expect(rules.length).toBe(expectedPackages.length);
+    it('has no duplicates', function() {
+      var rules = css.split('\n').filter(Boolean);
+      var expectedPackages = ['tires', 'brakes', 'calipers', 'drums', 'delorean', 'mr-fusion', 'focus', 'f150', 'truck-tires', 'cowboy-hat', 'truck-bed', 'gate', 'timeTravel', '88-mph'];
+
+      for(var i = 0; i < expectedPackages.length; i++) {
+        expect(rules).toContain('.' + expectedPackages[i] + ' {color: red}');
+      }
+      expect(rules.length).toBe(expectedPackages.length);
+    });
+
+    it('inlines the css in the right order', function() {
+      expect(css).toHaveOrder('drums', 'brakes');
+      expect(css).toHaveOrder('calipers', 'brakes');
+      expect(css).toHaveOrder('mr-fusion', 'delorean');
+      expect(css).toHaveOrder('brakes', 'delorean');
+      expect(css).toHaveOrder('brakes', 'focus');
+      expect(css).toHaveOrder('gate', 'truck-bed');
+      expect(css).toHaveOrder('cowboy-hat', 'f150');
+      expect(css).toHaveOrder('truck-bed', 'f150');
+      expect(css).toHaveOrder('truck-tires', 'f150');
+      expect(css).toHaveOrder('88-mph', 'timeTravel');
+      expect(css).toHaveOrder('delorean', 'timeTravel');
+    });
   });
 
-  it('inlines the css in the right order', function() {
-    expect(css).toHaveOrder('drums', 'brakes');
-    expect(css).toHaveOrder('calipers', 'brakes');
-    expect(css).toHaveOrder('mr-fusion', 'delorean');
-    expect(css).toHaveOrder('brakes', 'delorean');
-    expect(css).toHaveOrder('brakes', 'focus');
-    expect(css).toHaveOrder('gate', 'truck-bed');
-    expect(css).toHaveOrder('cowboy-hat', 'f150');
-    expect(css).toHaveOrder('truck-bed', 'f150');
-    expect(css).toHaveOrder('truck-tires', 'f150');
-    expect(css).toHaveOrder('88-mph', 'timeTravel');
-    expect(css).toHaveOrder('delorean', 'timeTravel');
+  describe('streaming', function() {
+    let files;
+    beforeEach(function() {
+      writeCode(path.resolve(__dirname, 'myApp', 'index.js'), () => {
+        require('../../index.js')({stream: true})
+          .pipe(require('through2').obj(function(file, encoding, callback){
+            console.log(file.path);
+            callback(null, file);
+        }));
+      });
+      files = childProcess.spawnSync('node', ['index.js'], {cwd: path.resolve(__dirname, 'myApp')})
+        .stdout.toString();
+    });
+
+    it('streams all the files', function() {
+      expect(files.trim()).toBe('components.css');
+    });
+  });
+
+  describe('directory', function() {
+    beforeEach(function() {
+      writeCode(path.resolve(__dirname, 'myApp', 'index.js'), () => {
+        require('../../index.js')({directory: 'tmp'})
+      });
+      childProcess.spawnSync('node', ['index.js'], {cwd: path.resolve(__dirname, 'myApp')});
+    });
+
+    it('writes the files', async function(done) {
+      var assetDir = path.resolve(__dirname, 'myApp', 'tmp');
+      expect(await readFile(path.join(assetDir, 'components.css'), 'utf8')).toContain('{color: red}');
+      done();
+    });
   });
 });
