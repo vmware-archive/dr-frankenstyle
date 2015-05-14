a2c471a2acce7b81659096d989da10d0eb4f776b
Vinson Chuong <vchuong@pivotal.io>
2015-05-14 14:33:18 -0700

    chore(ci): Remove missing file  from gulp lint task
    Signed-off-by: Nicole Sullivan <nsullivan@pivotal.io>

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('lint', function() {
-  return gulp.src(['gulpfile.js', 'tasks/**/*.js', 'src/**/*.js', 'spec/**/*.js', '!src/lib/console.js'])
+  return gulp.src(['gulpfile.js', 'tasks/**/*.js', 'src/**/*.js', 'spec/**/*.js'])
     .pipe(plugins.plumber())
     .pipe(plugins.eslint())
     .pipe(plugins.eslint.format('stylish'))
