9811418bc7619f10522342b8eb0c7b360f1cb7c2
Geoff Pleiss <gpleiss@pivotal.io>
2015-05-21 09:27:34 -0700

    Document rails option in CLI tool
    Signed-off-by: Vinson Chuong <vchuong@pivotal.io>

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,11 @@ import {dest} from 'vinyl-fs';
 import drFrankenstyle from './dr-frankenstyle';
 
 const argv = require('yargs')
-  .usage('Usage: dr-frankenstyle <output-dir>')
+  .usage('Usage: dr-frankenstyle [options] <output-dir>')
   .example('dr-frankenstyle my_assets_dir')
   .demand(1, 'Please provide an output directory')
   .boolean('rails')
+  .describe('rails', "Use Rails' asset-url helper instead of url in CSS")
   .argv;
 
 const output = argv._[0];
