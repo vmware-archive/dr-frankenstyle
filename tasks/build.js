36c40277e96982d999c237c7558db7651719d742
Geoff Pleiss <gpleiss@pivotal.io>
2015-05-21 10:15:14 -0700

    chore(build): create built gulp package
    [#94020828]
    
    Signed-off-by: Vinson Chuong <vchuong@pivotal.io>

--- tasks/build.js
+++ tasks/build.js
@@ -8,7 +8,7 @@ var plugins = require('gulp-load-plugins')();
 const COPYRIGHT = '//(c) Copyright 2015 Pivotal Software, Inc. All Rights Reserved.\n';
 
 gulp.task('clean', function(callback) {
-  del('dist', callback);
+  del(['dist', 'distGulp'], callback);
 });
 
 gulp.task('build', ['clean'], function() {
@@ -25,4 +25,21 @@ gulp.task('build', ['clean'], function() {
     }),
     gulp.src(['LICENSE', 'README.md', 'package.json'])
   ).pipe(gulp.dest('dist'));
+});
+
+gulp.task('buildGulp', ['build'], function() {
+  highland(gulp.src(['dist/LICENSE', 'GULP_README.md', 'package.json', 'dist/!(cli).js']))
+    .map(function(file) {
+      if (path.basename(file.path) === 'package.json') {
+        let newPackageInfo = JSON.parse(file.contents.toString());
+        newPackageInfo.name = 'gulp-dr-frankenstyle';
+        delete newPackageInfo.preferGlobal;
+        delete newPackageInfo.bin;
+        file.contents = new Buffer(JSON.stringify(newPackageInfo, null, 2));
+      } else if (path.basename(file.path) === 'GULP_README.md') {
+        file.path = path.join(path.dirname(file.path), 'README.md');
+      }
+      return file;
+    })
+    .pipe(gulp.dest('distGulp'));
 });
