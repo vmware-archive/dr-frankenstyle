c7da76dac4808062b002d08ec07050758272d1c7
Geoff Pleiss <gpleiss@pivotal.io>
2015-05-21 12:28:03 -0700

    chore(packaging): Release v0.1.0
    - Update publish task to publish both dr-frankenstyle and
    gulp-dr-frankenstyle
    
    [finishes #94020828]
    
    Signed-off-by: Vinson Chuong <vchuong@pivotal.io>

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var npm = require('npm');
 
-gulp.task('publish', ['build'], function(){
+gulp.task('publish', ['build', 'buildGulp'], function(){
   npm.load({}, function(error) {
     if (error) return console.error(error);
     npm.commands.publish(['dist'], function(error) {
       if (error) return console.error(error);
+      npm.commands.publish(['distGulp'], function(error) {
+        if (error) return console.error(error);
+      });
     });
   });
 });
