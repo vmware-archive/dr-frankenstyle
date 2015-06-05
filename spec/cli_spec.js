61296728aadeba9297835e3845dc32257947690e
Geoff Pleiss and Matt Royal <mroyal@pivotal.io>
2015-06-05 16:43:56 -0700

    fix(dependencies): lock down dependencies to fix Dr. F errors
    [Finishes #96313328]

--- spec/cli_spec.js
+++ spec/cli_spec.js
@@ -4,20 +4,16 @@ import {exec} from 'child-process-promise';
 import {generateProject, cleanupProject} from './helpers/dummy_project';
 import {toHaveOrder, toBeAFile} from './helpers/jasmine_matchers';
 
-import install from 'jasmine-es6/dist/overrides/async';
-import {catchError} from 'jasmine-es6';
-install();
-
 const command = `babel-node ${path.join(__dirname, '..', 'src', 'cli.js')}`;
 const expectedPackages = ['tires', 'brakes', 'calipers', 'drums', 'delorean', 'mr-fusion', 'focus', 'f150', 'truck-tires', 'cowboy-hat', 'truck-bed', 'gate', 'timeTravel', '88-mph'];
 const originalWorkingDirectory = process.cwd();
 
-async function cli(args = '') {
-  return await exec(`${command} ${args}`);
+function cli(args = '') {
+  return exec(`${command} ${args}`);
 }
 
-async function readCss() {
-  return await fs.readFile(path.resolve('public', 'components.css'), 'utf8');
+function readCss() {
+  return fs.readFile(path.resolve('public', 'components.css'), 'utf8');
 }
 
 describe('dr-frankenstyle', function() {
@@ -34,9 +30,13 @@ describe('dr-frankenstyle', function() {
   });
 
   describe('when called with no arguments', function() {
-    it('shows an error message', async function() {
-      const output = await catchError(cli());
-      expect(output).toContain('Please provide an output directory');
+    it('shows an error message', function(done) {
+      cli()
+        .then(() => { throw new Error("Expected an error, but got none") })
+        .catch(output => {
+          expect(output.stderr).toContain('Please provide an output directory');
+          done();
+        });
     });
   });
 
@@ -49,65 +49,79 @@ describe('dr-frankenstyle', function() {
       }
     });
 
-    it('inlines the css in the right order', async function() {
-      const css = await readCss();
-      expect(css).toHaveOrder('drums', 'brakes');
-      expect(css).toHaveOrder('calipers', 'brakes');
-      expect(css).toHaveOrder('mr-fusion', 'delorean');
-      expect(css).toHaveOrder('brakes', 'delorean');
-      expect(css).toHaveOrder('brakes', 'focus');
-      expect(css).toHaveOrder('gate', 'truck-bed');
-      expect(css).toHaveOrder('cowboy-hat', 'f150');
-      expect(css).toHaveOrder('truck-bed', 'f150');
-      expect(css).toHaveOrder('truck-tires', 'f150');
-      expect(css).toHaveOrder('88-mph', 'timeTravel');
-      expect(css).toHaveOrder('delorean', 'timeTravel');
+    it('inlines the css in the right order', function(done) {
+      readCss().then(css => {
+        expect(css).toHaveOrder('drums', 'brakes');
+        expect(css).toHaveOrder('calipers', 'brakes');
+        expect(css).toHaveOrder('mr-fusion', 'delorean');
+        expect(css).toHaveOrder('brakes', 'delorean');
+        expect(css).toHaveOrder('brakes', 'focus');
+        expect(css).toHaveOrder('gate', 'truck-bed');
+        expect(css).toHaveOrder('cowboy-hat', 'f150');
+        expect(css).toHaveOrder('truck-bed', 'f150');
+        expect(css).toHaveOrder('truck-tires', 'f150');
+        expect(css).toHaveOrder('88-mph', 'timeTravel');
+        expect(css).toHaveOrder('delorean', 'timeTravel');
+        done();
+      });
     });
 
-    it('has no duplicates', async function() {
-      const css = await readCss();
-      const rules = css.split('\n').filter(Boolean);
-      expect(rules.length).toBe(expectedPackages.length);
+    it('has no duplicates', function(done) {
+      readCss().then(css => {
+        const rules = css.split('\n').filter(Boolean);
+        expect(rules.length).toBe(expectedPackages.length);
 
-      for (const expectedPackage of expectedPackages) {
-        expect(rules).toContain(jasmine.stringMatching(`\\.${expectedPackage} \\{`));
-      }
+        for (const expectedPackage of expectedPackages) {
+          expect(rules).toContain(jasmine.stringMatching(`\\.${expectedPackage} \\{`));
+        }
+        done();
+      });
     });
   }
 
-  describe('normal use case', async function() {
-    beforeEach(async function() {
-      const {stdout, stderr} = await cli('public/');
-      if (stdout.toString().trim().length) console.log(stdout.toString());
-      if (stderr.toString().trim().length) console.log(stderr.toString());
+  describe('normal use case', function() {
+    beforeEach(function(done) {
+      cli('public/')
+        .then(function({stdout, stderr}) {
+          if (stdout) console.log(stdout);
+          if (stderr) console.error(stderr);
+          done();
+        })
+        .catch(function(err) {
+          throw err;
+        });
     });
 
     itProducesTheExpectedOutput();
 
-    it('puts assets in subdirectories', async function() {
-      const css = await readCss();
-      const rules = css.split('\n').filter(Boolean);
+    it('puts assets in subdirectories', function(done) {
+      readCss().then(css => {
+        const rules = css.split('\n').filter(Boolean);
 
-      for (const expectedPackage of expectedPackages) {
-        expect(rules).toContain(`.${expectedPackage} {background: url('${expectedPackage}/${expectedPackage}.png')}`);
-      }
+        for (const expectedPackage of expectedPackages) {
+          expect(rules).toContain(`.${expectedPackage} {background: url('${expectedPackage}/${expectedPackage}.png')}`);
+        }
+        done();
+      });
     });
   });
 
   describe('with Rails asset-urls', function() {
-    beforeEach(async function() {
-      await cli('--rails public/');
+    beforeEach(function(done) {
+      cli('--rails public/').then(done);
     });
 
     itProducesTheExpectedOutput();
 
-    it('puts assets in subdirectories and uses the asset-url helper', async function() {
-      const css = await readCss();
-      const rules = css.split('\n').filter(Boolean);
+    it('puts assets in subdirectories and uses the asset-url helper', function(done) {
+      readCss().then(css => {
+        const rules = css.split('\n').filter(Boolean);
 
-      for (const expectedPackage of expectedPackages) {
-        expect(rules).toContain(`.${expectedPackage} {background: asset-url('${expectedPackage}/${expectedPackage}.png')}`);
-      }
+        for (const expectedPackage of expectedPackages) {
+          expect(rules).toContain(`.${expectedPackage} {background: asset-url('${expectedPackage}/${expectedPackage}.png')}`);
+        }
+        done();
+      });
     });
   });
 });
